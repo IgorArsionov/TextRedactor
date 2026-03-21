f75ca0b2c3e7583a85648591262faebe5afdfc04
IgorArsionov <valisgvalis@gmail.com>
2026-03-21 18:05:45 +0200

    save

---
+++ src/main/java/com/example/textredactor/engine/exception/MappingProcessException.java
@@ -0,0 +1,7 @@
+package com.example.textredactor.engine.exception;
+
+public class MappingProcessException extends RuntimeException {
+    public MappingProcessException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/example/textredactor/engine/handler/mappers/AbstractMapper.java
+++ src/main/java/com/example/textredactor/engine/handler/mappers/AbstractMapper.java
@@ -1,11 +1,31 @@
 package com.example.textredactor.engine.handler.mappers;
 
+import com.example.textredactor.engine.exception.MappingProcessException;
+
 public abstract class AbstractMapper<T> implements Mapper {
     protected T currentClass;
 
 
 
     public void map(String value) {
+        if (value == null) {
+            throw new MappingProcessException("Value cannot be null");
+        }
+        String parameter = value.trim();
+        if (parameter.equals("{")) {
+            //TODO: check if it exists
+            currentClass = createObject();
+        }
+
+        if (parameter.equals("}")) {
+            //TODO: check if it already not exists
+            currentClass = null;
+        }
+
+        apply(parameter);
 
     }
+
+    protected abstract void apply(String value);
+    protected abstract T createObject();
 }
